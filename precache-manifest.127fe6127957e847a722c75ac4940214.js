666d375135f2a9b5144e7a1ba047dbbb8a43285a
LucasMJuarez <lumas89@gmail.com>
2020-05-19 21:43:12 -0300

    Updates

---
+++ precache-manifest.127fe6127957e847a722c75ac4940214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6c39d5d5667424d60dfd08bc1883b8",
+    "url": "/Portfolio-one/index.html"
+  },
+  {
+    "revision": "76580590dc0c1ac82bf3",
+    "url": "/Portfolio-one/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6b85bb0dbc459ef72c44",
+    "url": "/Portfolio-one/static/js/2.b9e4323c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-one/static/js/2.b9e4323c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76580590dc0c1ac82bf3",
+    "url": "/Portfolio-one/static/js/main.e4f32cf8.chunk.js"
+  },
+  {
+    "revision": "c65a3102e796c6649919",
+    "url": "/Portfolio-one/static/js/runtime-main.148ea7bf.js"
+  }
+]);
